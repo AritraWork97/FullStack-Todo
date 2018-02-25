321c42bac088cfd3b4f95d464a16a34df003a821
Aritra <aritraWorkBanerjee@gmail.com>
2018-02-25 22:01:34 +0530

    Working App Service File

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './AUTHENTICATION/signin/signin.component';
+import { SignupComponent } from './AUTHENTICATION/signup/signup.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { appRoutingModule } from './app-routing.module';
+import { todoService } from './SERVICES AND MODELS/todo.service'
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    LandingpageComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    appRoutingModule
   ],
-  providers: [],
+  providers: [
+    todoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
